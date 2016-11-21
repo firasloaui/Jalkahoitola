1635f8e1d655405c337208974f5343108d1c2f80
Firas <Firas@DESKTOP-JTM0R5R>
2016-11-21 21:50:49 +0200

    creat model, productgroups controller and productgroups view

---
+++ JalkaHoitola/JalkaHoitola/Controllers/ProductGroupsController.cs
@@ -0,0 +1,22 @@
+﻿using JalkaHoitola.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JalkaHoitola.Controllers
+{
+    public class ProductGroupsController : Controller
+    {
+        // GET: ProductGroups
+        public ActionResult Index()
+        {
+            JalkahoitolaEntities1 entities = new JalkahoitolaEntities1();
+            List<ProductGroups> model = entities.ProductGroups.ToList();
+            entities.Dispose();
+
+            return View(model);
+        }
+    }
+}
